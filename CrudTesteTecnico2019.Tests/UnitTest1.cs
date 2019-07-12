6915818977918053b85c02154de44910c72ebafc
Alexandre Del Picolo <delpicolo.alexandre@gmail.com>
2019-07-11 21:18:31 -0300

    Iniciado projeto de testes

---
+++ CrudTesteTecnico2019.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrudTesteTecnico2019.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
