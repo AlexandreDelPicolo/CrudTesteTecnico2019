b16a5cb9a3be6c04b11a3f01dd5db2a09973fcbc
Alexandre Del Picolo <adpicolo@voegol.com.br>
2020-06-17 20:28:45 -0300

    Teste sonar

--- CrudTesteTecnico2019.Web/Controllers/UsuarioController.cs
+++ CrudTesteTecnico2019.Web/Controllers/UsuarioController.cs
@@ -9,44 +9,46 @@ using System.Threading.Tasks;
 
 namespace CrudTesteTecnico2019.Web.Controllers
 {
-    [Route("v1/usuario")]
-    [ApiController]
-    public class UsuarioController : ControllerBase
+  [Route("v1/usuario")]
+  [ApiController]
+  public class UsuarioController : ControllerBase
+  {
+    private readonly IMediator _mediator;
+
+    public UsuarioController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
-
-        public UsuarioController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
-        [HttpPut("{id}")]
-        public async Task<CommandResult> Atualizar([FromRoute] long id, [FromBody] AtualizarUsuarioCommand command)
-        {
-            command.Id = id;
-
-            return await _mediator.Send(command);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<CommandResult> Excluir([FromRoute] long id)
-        {
-            var command = new ExcluirUsuarioCommand { Id = id };
-
-            return await _mediator.Send(command);
-        }
-
-        [HttpPost]
-        public async Task<CommandResult> Inserir([FromBody] InserirUsuarioCommand command)
-        {
-            return await _mediator.Send(command);
-        }
-
-
-        [HttpGet]
-        public async Task<IEnumerable<UsuarioModel>> Listar()
-        {
-            return await _mediator.Send(new ListarUsuarioQuery());
-        }
+      _mediator = mediator;
     }
+
+    [HttpPut("{id}")]
+    public async Task<CommandResult> Atualizar([FromRoute] long id, [FromBody] AtualizarUsuarioCommand command)
+    {
+      command.Id = id;
+
+      return await _mediator.Send(command);
+    }
+
+
+
+    [HttpDelete("{id}")]
+    public async Task<CommandResult> Excluir([FromRoute] long id)
+    {
+      var command = new ExcluirUsuarioCommand { Id = id };
+
+      return await _mediator.Send(command);
+    }
+
+    [HttpPost]
+    public async Task<CommandResult> Inserir([FromBody] InserirUsuarioCommand command)
+    {
+      return await _mediator.Send(command);
+    }
+
+
+    [HttpGet]
+    public async Task<IEnumerable<UsuarioModel>> Listar()
+    {
+      return await _mediator.Send(new ListarUsuarioQuery());
+    }
+  }
 }
